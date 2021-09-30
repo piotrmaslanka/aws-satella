70131e5bf267294561bb8342a8681269d8f9fa22
Piotr Maślanka <pmaslanka@smok.co>
2021-09-30 21:49:44 +0200

    fix export

--- aws_satella/exporter.py
+++ aws_satella/exporter.py
@@ -7,7 +7,6 @@ from boto3.exceptions import Boto3Error
 from botocore.exceptions import BotoCoreError, ClientError
 from satella.coding import reraise_as
 from satella.coding.concurrent import IntervalTerminableThread
-from satella.coding.transforms import stringify
 from satella.instrumentation.metrics import getMetric, MetricData
 from satella.time import parse_time_string
 
@@ -40,7 +39,7 @@ class AWSSatellaExporterThread(IntervalTerminableThread):
                  call_on_metric_upload_fails: tp.Callable[[Exception], None] = lambda e: None,
                  call_on_discarded_metric: tp.Callable[[MetricData], None] = lambda e: None):
         super().__init__(parse_time_string(interval), name='aws-satella-metrics', daemon=True)
-        self.MAX_SEND_AT_ONCE = max_send_at_once
+        self.max_send_at_once = max_send_at_once
         with reraise_as((Boto3Error, BotoCoreError, ClientError), InitializationError):
             self.cloudwatch = boto3.client('cloudwatch')
         self.call_on_metric_upload_fails = call_on_metric_upload_fails
@@ -70,7 +69,7 @@ class AWSSatellaExporterThread(IntervalTerminableThread):
                             'Unit': 'None',
                             'Value': val.value})
 
-            if len(results) >= self.MAX_SEND_AT_ONCE:
+            if len(results) >= self.max_send_at_once:
                 self.send_metrics(results)
                 results = []
         if results:
@@ -105,4 +104,3 @@ def start_if_not_started(*args, **kwargs) -> None:
         if worker_thread is None:
             worker_thread = AWSSatellaExporterThread(*args, **kwargs)
             worker_thread.start()
-
